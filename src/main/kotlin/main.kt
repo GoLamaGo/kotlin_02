dcc114e44e604a966875a0f0bd939b145e6baf7a
Andrew <aka.slurm@gmail.com>
2020-09-01 17:00:28 +0300

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,45 @@
+package ru.netology
+
+import kotlin.math.roundToInt
+
+fun main() {
+
+    //Task #1
+
+    println("Task #1\nВведите сумму перевода в копейках")
+    val fee = 0.0075
+    val amount: Long = readLine()!!.toLong() //в копейках
+    val transferFee = if (fee * amount <= 3500) 3500 else (amount * fee).roundToInt()
+
+    println("Комиссия за перевод составит: $transferFee коп")
+
+
+    //Task #2
+
+    println("\nTask #1\nВведите количество лайков")
+    val likes: Int = readLine()!!.toInt()
+    val hintText = if (likes % 10 == 1) "Человеку" else "Людям"
+
+    println("Запись понравилась $likes $hintText")
+
+
+    //Task #3
+
+    println("\nTask #3\nВведите сумму покупки")
+    val purchaseAmount = readLine()!!.toInt()
+
+    println("\nВведите сумму предыдущих покупок")
+    val previousPurchasesAmount = readLine()!!.toInt()
+    val previousPurchasesDiscount: Double = when {
+        previousPurchasesAmount <= 1000 -> purchaseAmount.toDouble()
+        previousPurchasesAmount > 1000 && previousPurchasesAmount <= 10000 -> (purchaseAmount - (purchaseAmount - 100)).toDouble()
+        else -> purchaseAmount * 0.05
+    }
+
+    println("\nЯвляется ли покупатель постоянным клиентом? (true / false)")
+    val melomanDiscount = if (readLine()!!.toBoolean() == true) purchaseAmount * 0.01 else purchaseAmount * 0.0
+
+    val finalCost = purchaseAmount - previousPurchasesDiscount - melomanDiscount
+
+    println(finalCost)
+}
